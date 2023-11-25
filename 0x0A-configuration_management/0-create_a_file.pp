9aaa94768114ff718b5a12e814cb40a6d8093417
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-11-24 21:49:27 -0800

    altered 0-created_a_fie.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,11 @@
-# Creates a file in /tmp
+# 0-create_a_file.pp
 
-
+# Define a file resource named 'school' with specified attributes
 file { '/tmp/school':
-	content =>'I love Puppet',
-	mode	=> '0744',
-	owner 	=> 'www-data',
-	group	=> 'www-data',
+  ensure   => present,   # Ensure the file exists
+  mode     => '0744',    # Set file permissions to 0744
+  owner    => 'www-data', # Set file owner to www-data
+  group    => 'www-data', # Set file group to www-data
+  content  => 'I love Puppet', # Set the content of the file
 }
+
