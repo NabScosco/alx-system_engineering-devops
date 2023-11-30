9b3f6200e66a09cc33b35842d273f4954a6f9ece
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-11-29 22:00:00 -0800

    Done with tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#using puppet to connet without password
+
+file { '/etc/ssh/ssh_config':
+	ensure -> present,
+}
+
+file_line {'Turn off passwd auth':
+  path	-> '/etc/ssh/ssh_config',
+  line	-> 'passwordAuthentication',
+  match	-> '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path	-> '/etc/ssh/ssh_config',
+  line	-> 'IdentityFile ~/.ssh/school',
+  match -> '^#IdentityFile',
+}
