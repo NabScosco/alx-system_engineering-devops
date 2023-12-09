1a532ac52af5bd7f9264232df2b72420bf248730
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-12-09 11:06:38 -0800

    puppet configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,7 +7,7 @@ file { '/etc/ssh/ssh_config':
 
 file_line {'Turn off passwd auth':
   path	-> '/etc/ssh/ssh_config',
-  line	-> 'passwordAuthentication',
+  line	-> 'passwordAuthentication no',
   match	-> '^#PasswordAuthentication',
 }
 
