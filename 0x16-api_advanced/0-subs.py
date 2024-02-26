b952ad236ccd449083d95da4e26d69169d3ae698
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-02-25 23:43:33 -0800

    changes to 0-subs file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """
-0-subs
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If the subreddit is invalid, it returns 0.
 """
 
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    and returns the number of subscribers
-    for a given subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'by u/qasqot79'}  # Set a custom User-Agent to avoid issues
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
 
-if __name__ == "__main__":
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+if __name__ == '__main__':
     import sys
 
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(num_subscribers)
