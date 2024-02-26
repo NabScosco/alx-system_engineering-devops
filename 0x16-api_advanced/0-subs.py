13a0ab081cb979c5fe3f4d6a95014533d25f16e5
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-02-25 23:14:40 -0800

    made changes to 0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+0-subs
 """
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Function that queries the Reddit API
+    and returns the number of subscribers
+    for a given subreddit.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return response.json().get('data', {}).get('subscribers', 0)
     else:
         return 0
+
+if __name__ == "__main__":
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
