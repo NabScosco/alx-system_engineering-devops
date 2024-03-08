629e4bfadc8cb35712089501aebfb29921b8a994
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2024-03-07 21:25:47 -0800

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-	"User-Agent": "linux:0x16.api.advanced:v1.o.o (by /u/bdov_)"
-	}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code ==404:
-	return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
+        return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
